ed42d03ee718ad34d5c456a1dde7b93b93774d98
khannaanurag1 <khannaanurag1@gmail.com>
2023-09-21 18:40:50 +0530

    dd

--- scripts.js
+++ scripts.js
@@ -1,9 +1,8 @@
-let apiKey = "AO48IFCXLA3BX1O9";
 let BaseUrl = "https://www.alphavantage.co/query?function={0}&symbol={1}&interval={2}&apikey=AO48IFCXLA3BX1O9";
 let Url = BaseUrl.replace("{0}","TIME_SERIES_INTRADAY").replace("{1}","MSFT").replace("{2}","1min");
 console.log(Url);
-async function callApi() {
-await fetch(Url)
+function callApi() {
+fetch(Url)
   .then(function(data) {
     const d = data.json();
     console.log(d);
@@ -12,5 +11,10 @@ await fetch(Url)
     console.error("There was a problem with the fetch operation:", error);
   });
 }
+let stockData;
+callApi().then(function(result){
+    console.log("2 "+result);
+   
+  })
 
-callApi();
+  console.log("3 "+stockData);
