20faecddde4590101b6a923a2855df2e4e03f537
khannaanurag1 <khannaanurag1@gmail.com>
2023-09-22 18:12:20 +0530

    Adding working API call

--- scripts.js
+++ scripts.js
@@ -1,61 +1,72 @@
-let BaseUrl = "https://www.alphavantage.co/query?function={0}&symbol={1}&interval={2}&apikey=AO48IFCXLA3BX1O9";
+let BaseUrl = "https://www.alphavantage.co/query?function={0}&symbol={1}&interval=60min&apikey=AO48IFCXLA3BX1O9";
 
-const sym = document.querySelector(".weight");
-const intraday = document.querySelector(".hight");
-const weekly = document.querySelector(".age");
-const daily = document.querySelector(".weight");
-const monthly = document.querySelector(".hight");
+const sym = document.querySelector(".symbol");
+const intraday = document.querySelector(".intraday");
+const weekly = document.querySelector(".weekly");
+const daily = document.querySelector(".daily");
+const monthly = document.querySelector(".monthly");
 let Url = null;
-<<<<<<< HEAD
-if (intraday != null && intraday != undefined) {
-  Url = BaseUrl.replace("{0}", "TIME_SERIES_INTRADAY").replace("{1}", sym).replace("{2}", "1min");
-} else if (weekly != null && weekly != undefined) {
-  Url = BaseUrl.replace("{0}", "TIME_SERIES_WEEKLY").replace("{1}", sym).replace("{2}", "1min");
-} else if (daily != null && daily != undefined) {
-  Url = BaseUrl.replace("{0}", "TIME_SERIES_DAILY").replace("{1}", sym).replace("{2}", "1min");
-} else if (monthly != null && monthly != undefined) {
-  Url = BaseUrl.replace("{0}", "TIME_SERIES_MONTHLY").replace("{1}", sym).replace("{2}", "1min");
-=======
 if(intraday != null && intraday != undefined) {
-  Url = BaseUrl.replace("{0}","TIME_SERIES_INTRADAY").replace("{1}",sym).replace("{2}","60min");
+  Url = BaseUrl.replace("{0}","TIME_SERIES_INTRADAY").replace("{1}",sym);
 } else if(weekly != null && weekly != undefined) {
-    Url = BaseUrl.replace("{0}","TIME_SERIES_WEEKLY").replace("{1}",sym).replace("{2}","60min");
+    Url = BaseUrl.replace("{0}","TIME_SERIES_WEEKLY").replace("{1}",sym);
 } else if(daily != null && daily != undefined) {
-    Url = BaseUrl.replace("{0}","TIME_SERIES_DAILY").replace("{1}",sym).replace("{2}","60min");
+    Url = BaseUrl.replace("{0}","TIME_SERIES_DAILY").replace("{1}",sym);
 } else if(monthly != null && monthly != undefined) {
-    Url = BaseUrl.replace("{0}","TIME_SERIES_MONTHLY").replace("{1}",sym).replace("{2}","60min");
+    Url = BaseUrl.replace("{0}","TIME_SERIES_MONTHLY").replace("{1}",sym);
 } else {
   Url = "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=MSFT&interval=60min&apikey=AO48IFCXLA3BX1O9";
->>>>>>> b662b9aba72efb3b280b141196d69795afdd0cc9
 }
 console.log(Url);
 
 let stockData = null;
 function callApi() {
-<<<<<<< HEAD
   fetch(Url)
-    .then(function (data) {
-      const d = data.json();
-      console.log("anur");
-      console.log(d);
-      console.log("khaa");
-      stockData = data["Meta Data"];
-      console.log("2 " + stockData);
-    })
-    .catch(function (error) {
-      console.error("There was a problem with the fetch operation:", error);
-    });
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    return response.json(); 
+  })
+  .then(data => {
+    stockData = data; 
+    console.log(stockData);
+    processStockData();
+  })
+  .catch(error => {
+      console.error('Fetch error:', error);
+  });
 }
-let stockData;
-callApi().then(function (result) {
-  stockData = result["Meta Data"];
+let marketopen = null;
+let marketlow = null;
+let markethigh = null;
+let marketvolume = null;
+let marketclose = null;
 
-  console.log("2 " + stockData);
+function processStockData() {
+  const timedstockData = stockData["Time Series (60min)"];
+  console.log(timedstockData);
+  const entries = Object.entries(timedstockData);
+  entries.forEach(Element => {
+    //console.log(Element);
+    //Processing each time stock element
+    const timekey = Element[0];
+    const timevalueobj = Element[1];
+    //console.log(timekey);
+    //console.log(timevalueobj);
+    marketopen = timevalueobj["1. open"];
+    marketclose = timevalueobj["4. close"];
+    marketlow = timevalueobj["3. low"];
+    markethigh = timevalueobj["2. high"];
+    marketvolume = timevalueobj["5. volume"];
+    console.log(marketvolume);
+  })
+}
 
-})
+callApi();
 
-console.log("3 " + stockData);
-=======
+/*
+function callApi() {
 fetch(Url)
   .then(function(data) {
     stockData = data.json();
@@ -95,7 +106,7 @@ callApi().then(function(result){
   //processStockData().then(function(result){
   //  console.log("anurag");
   //})
+  */
 
   
 
->>>>>>> b662b9aba72efb3b280b141196d69795afdd0cc9
