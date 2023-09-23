394bbf2584bbc89fb28f88f2ef561287f35513d6
khannaanurag1 <khannaanurag1@gmail.com>
2023-09-24 00:38:30 +0530

    final working code of API

--- scripts.js
+++ scripts.js
@@ -36,7 +36,6 @@ function getStockData() {
   }
 }
 
-
 let stockData = null;
 function callApi() {
   fetch(Url)
@@ -49,44 +48,91 @@ function callApi() {
   .then(data => {
     stockData = data; 
     console.log(stockData);
-    processStockData();
+    processStockDataAtOnce();
   })
   .catch(error => {
       console.error('Fetch error:', error);
   });
 }
+
+let timekey = null;
 let marketopen = null;
 let marketlow = null;
 let markethigh = null;
 let marketvolume = null;
 let marketclose = null;
+const timekeyvalues=[];
+const marketopenvalues=[];
+const marketclosevalues=[];
+const marketlowvalues=[];
+const markethighvalues=[];
+const marketvolumevalues=[];
 
-function processStockData() {
-  const timedstockData = stockData["Time Series (60min)"];
+
+async function processStockDataAtOnce() {
+  const timedstockData = await stockData["Time Series (60min)"];
   console.log(timedstockData);
   const entries = Object.entries(timedstockData);
   entries.forEach(Element => {
-    //console.log(Element);
-    //Processing each time stock element
-    const timekey = Element[0];
+    timekey = Element[0];
+    timekeyvalues.push(timekey);
     const timevalueobj = Element[1];
-    //console.log(timekey);
-    //console.log(timevalueobj);
     marketopen = timevalueobj["1. open"];
+    marketopenvalues.push(marketopen);
     marketclose = timevalueobj["4. close"];
+    marketclosevalues.push(marketclose);
     marketlow = timevalueobj["3. low"];
+    marketlowvalues.push(marketlow);
     markethigh = timevalueobj["2. high"];
+    markethighvalues.push(markethigh);
     marketvolume = timevalueobj["5. volume"];
-    console.log(marketvolume);
-    document.querySelector('.time').innerHTML = timekey;
-    document.querySelector('.open').innerHTML = marketopen;
-    document.querySelector('.low').innerHTML = marketlow;
-    document.querySelector('.high').innerHTML = markethigh;
-    document.querySelector('.close').innerHTML = marketclose;
-    document.querySelector('.volume').innerHTML = marketvolume;
+    marketvolumevalues.push(marketvolume);
+  });
 
-  })
+  var table = document.getElementById("stocktable");
+  //for(var i=0;i<marketclosevalues.length;i++) {
+    for(var i=0;i<5;i++) {
+    var row = table.insertRow(i+1);
+    var cell0 = row.insertCell(0);
+    var cell1 = row.insertCell(1);
+    var cell2 = row.insertCell(2);
+    var cell3 = row.insertCell(3);
+    var cell4 = row.insertCell(4);
+    var cell5 = row.insertCell(5);
+
+    cell0.innerHTML = timekeyvalues[i];
+    cell1.innerHTML = marketopenvalues[i];
+    cell2.innerHTML = marketlowvalues[i];
+    cell3.innerHTML = markethighvalues[i];
+    cell4.innerHTML = marketclosevalues[i];
+    cell5.innerHTML = marketvolumevalues[i];
+  }
 }
+
+/** 
+  async function processStockData() {
+    const timedstockData = stockData["Time Series (60min)"];
+    console.log(timedstockData);
+    const entries = Object.entries(timedstockData);
+    entries.forEach(Element => {
+      const timekey = Element[0];
+      const timevalueobj = Element[1];
+      marketopen = timevalueobj["1. open"];
+      marketclose = timevalueobj["4. close"];
+      marketlow = timevalueobj["3. low"];
+      markethigh = timevalueobj["2. high"];
+      marketvolume = timevalueobj["5. volume"];
+      console.log(marketvolume);
+      document.querySelector('.time').innerHTML = timekey;
+      document.querySelector('.open').innerHTML = marketopen;
+      document.querySelector('.low').innerHTML = marketlow;
+      document.querySelector('.high').innerHTML = markethigh;
+      document.querySelector('.close').innerHTML = marketclose;
+      document.querySelector('.volume').innerHTML = marketvolume;
+    });
+  }
+  **/
+
 
 
   
